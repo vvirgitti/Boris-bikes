e8136dad75f59d12410e5e7dad0726dc55ecbbff
Vanessa <virgitti@gmail.com>
2015-02-17 23:41:44 +0000

    added station class and tests on docking a bike and number

--- lib/station.rb
+++ lib/station.rb
@@ -1,16 +1,26 @@
 class Station
 
 
+  def initialize
+    @bikes = []
+    @capacity = 10
+  end
+
+
   def dock(bike)
-    bike =+ 1
-  end
-
-  def bikes
-    bikes << bike
+    raise 'The station is full' if full?
+    @bikes << bike
   end
 
 
+  def bike_count
+    @bikes.count
+  end
 
+
+  def full?
+    bike_count >= @capacity
+  end
 
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -5,14 +5,25 @@ describe Station do
   let(:station){Station.new}
   let(:bike){double :bike, :broken => false}
 
-  it 'can accept a bike' do
-    expect(station.dock(bike)).to eq 1
+  it 'can dock a bike' do
+    expect{station.dock(bike)}.to change{station.bike_count}.by 1
   end
 
 
-  it 'can accept a bike' do
-    station.dock(bike)
-    expect(station.dock(bike)).to eq 2
+  it 'knows how many bikes are in the station' do
+    2.times {station.dock(bike)}
+    expect(station.bike_count).to eq 2
+  end
+
+
+  it 'knows that it is full' do
+    20.times {station.dock(bike)}
+    expect(station).to be_full
+  end
+
+  it 'cannot accept any more bikes if it is full' do
+    20.times {station.dock(bike)}
+    expect{station.dock(bike)}.to raise_error('The station is full')
   end
 
 
